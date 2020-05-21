eaa9bc9fe296d80be3540d455704f55c8e3d0605
GorAraqelyan <50023373+GorAraqelyan@users.noreply.github.com>
2020-05-21 13:13:22 +0400

    Updates

---
+++ precache-manifest.e538776064578d7cc2d1717a18e0e5df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e78425594d7a654ee79b50546e46e5",
+    "url": "/table-with-filters/index.html"
+  },
+  {
+    "revision": "edf59dd93ccf43a8f67f",
+    "url": "/table-with-filters/static/css/main.c2f6f955.chunk.css"
+  },
+  {
+    "revision": "76a8d8d37b6f95752743",
+    "url": "/table-with-filters/static/js/2.197eda89.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/table-with-filters/static/js/2.197eda89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf59dd93ccf43a8f67f",
+    "url": "/table-with-filters/static/js/main.245fcd40.chunk.js"
+  },
+  {
+    "revision": "967823f12b415d4f1bf9",
+    "url": "/table-with-filters/static/js/runtime-main.ea86753d.js"
+  }
+]);
